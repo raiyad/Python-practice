3b517a7d2af0bc7e04950a0f0a1a20991e28b27a
Raiyad Alam <52122570+raiyad@users.noreply.github.com>
2022-05-13 03:26:42 -0700

    Raiyad

---
+++ 01.Data types.py
@@ -0,0 +1,21 @@
+# Fundamental Data types
+from sys import modules
+
+
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+# Classes-> custom types
+
+
+# specialized Data types
+
+modules
+
+None
---
+++ 02.Numbers.py
@@ -0,0 +1,7 @@
+print(2+4)
+print(2-4)
+print(type(2/4))
+print(type(2*4))
+print(2**2)
+print(2//4)
+print(4 % 6)
